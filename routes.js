247a14ef69a66a7a0d28efe7b5cbe99ff734359f
ilma.nur <ilma.nur@bluebirdgroup.com>
2023-05-29 11:40:03 +0700

    add responses

--- routes.js
+++ routes.js
@@ -24,7 +24,33 @@ const routes = [
     method: '*',
     path: '/about',
     handler: (request, h) => {
-        return `Site can't be accessed using this method`;
+      return `Site can't be accessed using this method`;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = 'stranger' } = request.params
+      const { lang } = request.query
+
+      if (lang === 'id') {
+        return `Selamat datang, ${name}!`
+      }
+
+      return `Hello ${name}!`
+    },
+  },
+  {
+    method: 'POST',
+    path: '/login',
+    handler: (request, h) => {
+      const { username, password } = request.payload
+
+      return h.response(`Welcome, ${username}!`)
+        .type('application/json')
+        .header('X-Powered-By', 'NodeJS')    
+        .code(201)      
     },
   },
   {
